b48e609dd045725335d4da55528b7272596046ab
RJ Jundt <rjjundt@gmail.com>
2015-10-10 13:27:15 -0600

    Initial game of life state can be provided via command-line arguments.

--- GameOfLife/main.cpp
+++ GameOfLife/main.cpp
@@ -1,15 +1,28 @@
 #include "GameOfLife.h"
+#include <regex>
 
 int main(int argc, char* argv[])
 {
-	GameOfLife gameOfLife;
-	gameOfLife.addLiveCell(0, 0);
-	gameOfLife.addLiveCell(1, 0);
-	gameOfLife.addLiveCell(2, 0);
-	gameOfLife.addLiveCell(-1, -1);
-	gameOfLife.addLiveCell(0, -1);
-	gameOfLife.addLiveCell(1, -1);
+	using namespace std;
 
+	GameOfLife gameOfLife;
+
+	// Add any pairs that match a standard coordinate style - e.g. (+10, -100)
+	regex coordMatcher("\\(([\\+|-]?[0-9]+),[ ]*([\\+|-]?[0-9]+)\\)");
+	for (int i = 1; i < argc; i++)
+	{
+		string coords = string(argv[i]);
+		smatch matchResults;
+
+		// Add a match 
+		if (regex_match(coords, matchResults, coordMatcher))
+		{
+			int64_t x = stoll(matchResults[1]);
+			int64_t y = stoll(matchResults[2]);
+			gameOfLife.addLiveCell(x, y);
+		}
+	}
+	
 	for (unsigned short i = 0; i < 10; i++)
 	{
 		gameOfLife.nextGeneration();
